1146eb1b660305acae0f7107c02a467f29bab49d
JuanKarlos Espinoza <jespinoza@domsf.com>
2018-09-05 12:01:49 -0300

    Init commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {PlacePage} from "../place/place";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  addPlacePage= PlacePage
 
   constructor(public navCtrl: NavController) {
 
